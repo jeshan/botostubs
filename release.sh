c75d683d8bfbf883f75c31aee4c794f890adeb87
jeshan <j@jeshan.co>
2018-11-30 01:02:58 +0400

    use env var

--- release.sh
+++ release.sh
@@ -3,5 +3,5 @@
 python setup.py sdist bdist_wheel
 cp -r dist /custom
 
-python -c "print('Sanity check'); import botostubs; print('Sanity check successful');" && twine upload --username `aws ssm get-parameter --name /CodeBuild/pypi-user --output text --query Parameter.Value` \
-  --password `aws ssm get-parameter --name /CodeBuild/pypi-password --output text --query Parameter.Value` dist/*
+python -c "print('Sanity check'); import botostubs; print('Sanity check successful');" && twine upload --username ${TWINE_USERNAME} \
+  --password ${TWINE_PASSWORD} dist/*
