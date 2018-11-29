70315831c0a565bf528e72acfd5f92ff55325dd2
jeshan <j@jeshan.co>
2018-11-29 23:51:50 +0400

    add sanity check before upload to PyPI

--- release.sh
+++ release.sh
@@ -3,5 +3,5 @@
 python setup.py sdist bdist_wheel
 cp -r dist /custom
 
-twine upload --username `aws ssm get-parameter --name /CodeBuild/pypi-user --output text --query Parameter.Value` \
+python -c "print('Sanity check'); import botostubs; print('Sanity check successful');" && twine upload --username `aws ssm get-parameter --name /CodeBuild/pypi-user --output text --query Parameter.Value` \
   --password `aws ssm get-parameter --name /CodeBuild/pypi-password --output text --query Parameter.Value` dist/*
