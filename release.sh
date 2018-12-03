a14ea5256cc116b33fb33eb1c0af36ed0f721b75
jeshan <j@jeshan.co>
2018-12-03 12:36:53 +0400

    try pass build if file already exists on PyPI

--- release.sh
+++ release.sh
@@ -3,5 +3,5 @@
 python setup.py sdist bdist_wheel
 cp -r dist /custom
 
-python -c "print('Sanity check'); import botostubs; print('Sanity check successful');" && twine upload --username ${TWINE_USERNAME} \
+python -c "print('Sanity check'); import botostubs; print('Sanity check successful');" && twine upload --skip-existing --username ${TWINE_USERNAME} \
   --password ${TWINE_PASSWORD} dist/*
