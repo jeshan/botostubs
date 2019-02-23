2ddbc05bc46df15ddfee6d5a4ffbd5590d2f3851
jeshan <j@jeshan.co>
2019-02-24 00:28:07 +0400

    fix path

--- release.sh
+++ release.sh
@@ -7,7 +7,7 @@ cp -r dist /custom
 python -c "print('Sanity check'); import botostubs; print('Sanity check successful');" && twine upload --skip-existing --username ${TWINE_USERNAME} \
   --password ${TWINE_PASSWORD} dist/*
 
-python post-release-data.py > data.json
+python ../post-release-data.py > data.json
 
 curl --header "Content-Type: application/json" \
   -u jeshan:${GITHUB_TOKEN} \
