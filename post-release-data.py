a56fd88bbf1c2c66ad5427dc024efd085923e129
jeshan <j@jeshan.co>
2019-02-24 00:38:55 +0400

    fix typo

--- post-release-data.py
+++ post-release-data.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 import boto3
 import os
-print(f'{{"tag_name": "v{os.environ["CODE_VERSION"]}.{boto3.__version__}", "name": "{os.environ["CODE_VERSION"]}.{boto3.__version__}", "body": "Compatibility with boto3 version {boto3.__version__}.See boto3 release here:\nhttps://github.com/boto/boto3/releases/tag/{boto3.__version__}"}}')
+print(f'{{"tag_name": "v{os.environ["CODE_VERSION"]}.{boto3.__version__}", "name": "{os.environ["CODE_VERSION"]}.{boto3.__version__}", "body": "Compatibility with boto3 version {boto3.__version__}.See boto3 release here: https://github.com/boto/boto3/releases/tag/{boto3.__version__}"}}')
 
