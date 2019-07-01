5d901eb9462f6861599cfdcc9285c6d9b7e122d0
jeshan <j@jeshan.co>
2019-07-01 23:53:44 +0400

    release 0.10: try to set correct bsd licence

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jeshan/botostubs",
     packages=setuptools.find_packages(),
+    license='BSD-2-Clause',
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
