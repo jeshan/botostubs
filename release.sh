97d34fcfe9b520b51f0077aa9c7d703b195fb1c5
jeshan <j@jeshan.co>
2020-01-26 00:46:29 +0400

    separate command so that script fails

--- release.sh
+++ release.sh
@@ -4,8 +4,8 @@ set -ex
 python setup.py sdist bdist_wheel
 cp -r dist /custom
 
-python -c "print('Sanity check'); import botostubs; print('Sanity check successful');" && twine upload --skip-existing --username ${TWINE_USERNAME} \
-  --password ${TWINE_PASSWORD} dist/*
+python -c "print('Sanity check'); import botostubs; print('Sanity check successful');"
+twine upload --skip-existing --username ${TWINE_USERNAME} --password ${TWINE_PASSWORD} dist/*
 
 python ../post-release-data.py > data.json
 
